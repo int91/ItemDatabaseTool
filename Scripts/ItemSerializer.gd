2c85f14dd22c95208286b15cd29f45dbb09c3662
int91 <chize199@gmail.com>
2021-12-30 16:28:21 -0500

    Updated source
    Exported .json files now save to a folder called "Exports"
    
    Updated .gitignore to ignore this "Exports" folder

--- Scripts/ItemSerializer.gd
+++ Scripts/ItemSerializer.gd
@@ -1,23 +1,31 @@
 extends Node
 
+var path = ProjectSettings.globalize_path("res://")
+var directory = path+"/Exports/"
 
 func _ready():
 	pass
 
+#TODO: Add a form of source control built in to exporting files (make this a setting too)
+
 func LoadItemData():
+	CreateExportDirectory()
 	var file = File.new()
-	file.open("res://items.json", File.READ)
-	var items = parse_json(file.get_line())
-	file.close()
-	for i in items:
-		var q = load("res://Scripts/Item.gd").new()
-		q.SetData(i.name, i.lore, i.weight, i.value, i.maxStack, i.type)
-		ItemDatabase.items.append(q)
+	if (file.file_exists(directory+"items.json")):
+		file.open(directory+"items.json", File.READ)
+		var items = parse_json(file.get_line())
+		file.close()
+		if (items != null):
+			for i in items:
+				var q = load("res://Scripts/Item.gd").new()
+				q.SetData(i.name, i.lore, i.weight, i.value, i.maxStack, i.type)
+				ItemDatabase.items.append(q)
 
 func SaveItemData():
+	CreateExportDirectory()
 	var file = File.new()
 	var it = []
-	file.open("res://items.json", File.WRITE)
+	file.open(directory+"items.json", File.WRITE)
 	for i in ItemDatabase.items:
 		var data = {
 			"name":i.GetName(),
@@ -29,3 +37,8 @@ func SaveItemData():
 		it.append(data)
 	file.store_line(to_json(it))
 	file.close()
+
+func CreateExportDirectory():
+	var dir = Directory.new()
+	if (!dir.dir_exists(path+"/Exports/")):
+		dir.make_dir(directory)
